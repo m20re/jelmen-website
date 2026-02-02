9d8ef98131b7018879a1061e274f424f0ce7ef65
m20re <marvinorellana@unomaha.edu>
2026-02-02 17:10:24 -0600

    Added Tailwind CSS and update layout structure

--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,11 @@
 // found at "/"
 export default function Page() {
-  return <h1>Hello, Next.js!</h1>;
+  return (
+    <div>
+      <nav className="min-w-full bg-black"></nav>
+      <main className="">
+        <h1 className="text-4xl font-bold">Welcome to Next.js!</h1>
+      </main>
+    </div>
+  );
 }
