8a54001b856c5459bc98d63a0e512ba02afd30d2
m20re <131278384+m20re@users.noreply.github.com>
2026-03-31 13:42:29 -0500

    added output to ensure project ID is being read

--- sanity/env.js
+++ sanity/env.js
@@ -2,5 +2,5 @@ export const apiVersion =
   process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2026-03-30'
 
 export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
-// Prevent build errors?
-export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "";
+console.log("PROJECT ID:", process.env.NEXT_PUBLIC_SANITY_PROJECT_ID);
+export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
